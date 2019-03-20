29ad5422b8ccc859cf775cb1af4f2f7832189d7b
AnthonyTIrwin <41561146+AnthonyTIrwin@users.noreply.github.com>
2019-03-20 19:54:59 -0400

    Team List

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp7
         {
 
             List<RosterData> ListRoster = new List<RosterData>();
-            using (TextReader reader = File.OpenText(@"C:\\temp\mlb_players.csv"))
+            using (TextReader reader = File.OpenText($"../../mlb_players.csv"))
             {
                 CsvReader csv = new CsvReader(reader);
                 csv.Configuration.Delimiter = ",";
@@ -32,6 +32,21 @@ namespace ConsoleApp7
                 }                
 
             }
+
+
+            Console.WriteLine("Please enter team name using initials from following list.");
+            Console.WriteLine("Baltimore Orieoles = BAL  Chicago White Sox    = CWS  California Angels = ANA");
+            Console.WriteLine("Boston Red Sox     = BOS  Cleveland Indians    = CLE  Oakland A's       = OAK");
+            Console.WriteLine("New York Yankees   = NYY  Detroit Tigers       = DET  Seattle Mainers   = SEA");
+            Console.WriteLine("Tampa Bay Rays     = TB   Kansas City Royals   = KC   Texas Rangers.........= TEX");
+            Console.WriteLine("Toronto Blue Jays  = TOR  Minnesota Twins      = MIN  Atlanta Braves........= ATL");
+            Console.WriteLine("Chicago Cubs       = CHC  Arizona Diamondbacks = ARZ  Miami Marlins         = FLA");
+            Console.WriteLine("Cincinnati Reds    = CIN  Colorado Rockies     = COL  New York Mets         = NYM");
+            Console.WriteLine("Houstan Astros     = HOU  LA Dodgers           = LA   Philidephia Phillies  = PHI");
+            Console.WriteLine("Millawakee Brewers = MLW  San Diego Padres     = SD   Washington Nationals  = WAS");
+            Console.WriteLine("Pittsburg Pirates  = PIT  San Francisco Giants = SF   Saint Louis Cardinals = STL");
+
+
             Console.WriteLine("Please input name of MLB Player:");
             var plnm = Console.ReadLine();
             int index = ListRoster.FindIndex(x => x.Name.Contains(plnm));
@@ -52,7 +67,7 @@ namespace ConsoleApp7
             Console.WriteLine("Age:      {0}", ListRoster[index].Age);
             Console.ReadLine();
 
-            using (var writer = new StreamWriter(@"c:\temp\csvThing.csv"))
+            using (var writer = new StreamWriter($"../../mlb_players.csv"))
             using (var csv = new CsvWriter(writer))
             {
                 csv.WriteRecords(ListRoster);
