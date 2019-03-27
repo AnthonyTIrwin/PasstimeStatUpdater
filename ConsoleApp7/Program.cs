ae4b31a7ec019d05026cea6c70f9350a7bb27907
AnthonyTIrwin <41561146+AnthonyTIrwin@users.noreply.github.com>
2019-03-27 18:10:07 -0400

    added comments

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -8,11 +8,17 @@ namespace PlayerManager
         {
             //Call UI Class
             UIClass OnTeam = new UIClass();
+            //converts .cvs file to list
             OnTeam.TeamRoster();
+            //sorts players from specific team
             OnTeam.TmSearch();
+            //shows player profile
             OnTeam.PlayerData();
+            //able to change player data
             OnTeam.PlayerChanger();
+            //shows updated list
             OnTeam.UpdatedList();
+            //saves updated list to .csv file
             OnTeam.SaveIt();
             Console.ReadLine();         
         }
